0bac3534b7813316c95d1d2734454f8e03e6b74c
mikeEngineer <m.kei9192@gmail.com>
2021-04-14 00:39:45 +0900

    記事更新画面と記事削除モーダル画面の作成 #11

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,21 +10,26 @@ class ArticleController extends Controller
 {
   public function index()
   {
-      $articles = Article::all()->sortByDesc('created_at');
+    $articles = Article::all()->sortByDesc('created_at');
 
-      return view('articles.index', ['articles' => $articles]);
+    return view('articles.index', ['articles' => $articles]);
   }
 
   public function create()
-    {
-        return view('articles.create');
-    }
+  {
+    return view('articles.create');
+  }
 
   public function store(ArticleRequest $request, Article $article)
-    {
-        $article->fill($request->all()); 
-        $article->user_id = $request->user()->id;
-        $article->save();
-        return redirect()->route('articles.index');
-    }
+  {
+    $article->fill($request->all());
+    $article->user_id = $request->user()->id;
+    $article->save();
+    return redirect()->route('articles.index');
+  }
+
+  public function edit(Article $article)
+  {
+    return view('articles.edit', ['article' => $article]);    
+  }
 }
